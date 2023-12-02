840eba84145d08811478e76632aa2650ad3a44e3
ZAKAR <saojung518@naver.com>
2023-12-03 01:27:37 +0900

    docs

---
+++ home.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="FullStackGPT Home",
+    page_icon="🤖"
+)
+
+st.markdown(
+    """
+# Hello!
+            
+Welcome to my FullstackGPT Portfolio!
+            
+Here are the apps I made:
+            
+- [x] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
